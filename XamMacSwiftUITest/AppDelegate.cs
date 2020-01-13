e5ec1ad3f0fdd118a7c65394b2b7b87169ab52ff
CartBlanche <savagesoftware@gmail.com>
2020-01-13 11:54:35 +0000

    Remove call to WillClose() in FShartp project. Also slight refactor to make sure App actually closes when last window closed.

--- XamMacSwiftUITest/AppDelegate.cs
+++ XamMacSwiftUITest/AppDelegate.cs
@@ -12,9 +12,8 @@ namespace XamMacSwiftUITest
 	{
 		public override void DidFinishLaunching (NSNotification notification)
 		{
-			var window = new NSWindow (new CGRect (x: 0, y: 0, width: 480, height: 300),
-				NSWindowStyle.Titled | NSWindowStyle.Closable | NSWindowStyle.Miniaturizable | NSWindowStyle.Resizable | NSWindowStyle.FullSizeContentView,
-				NSBackingStore.Buffered, deferCreation: false);
+			var flags = NSWindowStyle.Titled | NSWindowStyle.Closable | NSWindowStyle.Miniaturizable | NSWindowStyle.Resizable | NSWindowStyle.FullSizeContentView;
+			var window = new NSWindow (new CGRect (x: 0, y: 0, width: 480, height: 300), flags, NSBackingStore.Buffered, deferCreation: false);
 
 			window.Center ();
 
@@ -22,5 +21,11 @@ namespace XamMacSwiftUITest
 
 			window.MakeKeyAndOrderFront (null);
 		}
-	}
+
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender)
+        {
+			GC.Collect();
+			return true;
+        }
+    }
 }
