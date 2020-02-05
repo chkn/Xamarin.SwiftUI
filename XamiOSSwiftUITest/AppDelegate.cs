e87537fc31efc4e0128ba91c50e50c3ddfd4dbb1
Alex Corrado <alexander.corrado@gmail.com>
2020-02-04 22:42:09 -0500

    [iOS] Remove fancy scene stuff and storyboard reference from iOS test project

--- XamiOSSwiftUITest/AppDelegate.cs
+++ XamiOSSwiftUITest/AppDelegate.cs
@@ -32,24 +32,6 @@ namespace XamiOSSwiftUITest
             Window.MakeKeyAndVisible ();
             return true;
         }
-
-        // UISceneSession Lifecycle
-
-        [Export("application:configurationForConnectingSceneSession:options:")]
-        public UISceneConfiguration GetConfiguration(UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
-        {
-            // Called when a new scene session is being created.
-            // Use this method to select a configuration to create the new scene with.
-            return UISceneConfiguration.Create ("Default Configuration", connectingSceneSession.Role);
-        }
-
-        [Export("application:didDiscardSceneSessions:")]
-        public void DidDiscardSceneSessions(UIApplication application, NSSet<UISceneSession> sceneSessions)
-        {
-            // Called when the user discards a scene session.
-            // If any sessions were discarded while the application was not running, this will be called shortly after `FinishedLaunching`.
-            // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-        }
     }
 }
 
