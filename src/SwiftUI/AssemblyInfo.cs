116c71e6d4839de64a95041ed77a1c636e52d721
Alex Corrado <alexander.corrado@gmail.com>
2020-03-29 19:12:52 -0400

    [build] Move to LinkWith and binding project
    This causes the native libs to be embedded in the assemblies,
    making everything simpler.

--- src/SwiftUI/AssemblyInfo.cs
+++ src/SwiftUI/AssemblyInfo.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
+#if __UNIFIED__
+using ObjCRuntime;
+#endif
+
 [assembly: InternalsVisibleTo ("SwiftUI.Tests")]
 
+#if __MACOS__
+[assembly: LinkWith ("libSwiftUIGlue.dylib", Dlsym = DlsymOption.Disabled)]
+#elif __UNIFIED__
+[assembly: LinkWith ("SwiftUIGlue.framework", Dlsym = DlsymOption.Disabled)]
+#endif
