d4e30ff0a583c9f967a14dd65e0ad1e1c8424149
CartBlanche <savagesoftware@gmail.com>
2020-05-06 19:41:54 +0100

    Add Tests to make sure Static Color Calls and HSBO Constructor calls work.

---
+++ tests/SwiftUI.Tests/ColorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace SwiftUI.Tests
+{
+    public class ColorTests : TestFixture
+    {
+        [Fact]
+        public void AllStaticColorCallsWork ()
+        {
+            Type t = typeof(Color);
+            var properties = t.GetProperties (BindingFlags.Public | BindingFlags.Static);
+			foreach (var item in properties) {
+                var value = item.GetValue (null);
+                Assert.NotNull (value);
+			}
+        }
+
+        [Fact]
+        public void ColorHSBOCallWorks ()
+        {
+            var colour = new Color (0.0f, 0.6f, 0.0f, 0.5f);
+
+            Assert.NotNull (colour);
+        }
+    }
+}
