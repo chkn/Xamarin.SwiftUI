09f0862418abacb6a27f53e43a52f18e04ba5b6b
CartBlanche <savagesoftware@gmail.com>
2020-05-06 20:22:41 +0100

    An extra check in case something changes and we get a EntryPointNotFoundException.

--- tests/SwiftUI.Tests/ColorTests.cs
+++ tests/SwiftUI.Tests/ColorTests.cs
@@ -9,20 +9,31 @@ namespace SwiftUI.Tests
         [Fact]
         public void AllStaticColorCallsWork ()
         {
-            Type t = typeof(Color);
-            var properties = t.GetProperties (BindingFlags.Public | BindingFlags.Static);
-			foreach (var item in properties) {
-                var value = item.GetValue (null);
-                Assert.NotNull (value);
-			}
+            // In case we get an EntryPointNotFoundException 
+            var exception = Record.Exception (() => {
+                Type t = typeof (Color);
+                var properties = t.GetProperties (BindingFlags.Public | BindingFlags.Static);
+                foreach (var item in properties) {
+                    var colour = item.GetValue (null);
+                    Assert.NotNull (colour);
+                };
+
+            });
+
+            Assert.Null (exception);
         }
 
         [Fact]
         public void ColorHSBOCallWorks ()
         {
-            var colour = new Color (0.0f, 0.6f, 0.0f, 0.5f);
+            // In case we get an EntryPointNotFoundException 
+            var exception = Record.Exception (() => {
+                var colour = new Color (0.0f, 0.6f, 0.0f, 0.5f);
 
-            Assert.NotNull (colour);
+                Assert.NotNull (colour);
+            });
+
+            Assert.Null (exception);
         }
     }
 }
