e2160daac87cedaa448a0516cea533003dcb6312
CartBlanche <savagesoftware@gmail.com>
2020-05-28 21:19:19 +0100

    Initial TVOS Support.

---
+++ src/SwiftUI/TVOS/Color.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using Foundation;
+using UIKit;
+
+using Swift.Interop;
+
+namespace SwiftUI
+{
+	public unsafe partial class Color
+	{
+		#region Constructors
+		public Color (UIColor color)
+		{
+			Data = CreateFromUIColor(color.Handle.ToPointer());
+		}
+
+		public Color (string name, NSBundle? bundle = null)
+		{
+			Data = CreateFromStringBundle (new Swift.String (name), bundle == null ? IntPtr.Zero.ToPointer() : bundle.Handle.ToPointer());
+		}
+		#endregion
+
+		#region DllImports
+		// Initialisers
+		[DllImport (SwiftUILib.Path,
+			CallingConvention = CallingConvention.Cdecl,
+			EntryPoint = "$s7SwiftUI5ColorVyACSo7UIColorCcfC")]
+		static extern IntPtr CreateFromUIColor (void* colorPointer);
+
+		// Initialisations
+		[DllImport(SwiftUILib.Path,
+			CallingConvention = CallingConvention.Cdecl,
+			EntryPoint = "$s7SwiftUI5ColorV_6bundleACSS_So8NSBundleCSgtcfC")]
+		static extern IntPtr CreateFromStringBundle (
+			Swift.String str,
+			void* bundlePointer);
+		#endregion
+	}
+}
---
+++ src/SwiftUI/TVOS/UIHostingView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using UIKit;
+
+using Swift;
+using Swift.Interop;
+
+namespace SwiftUI
+{
+    public unsafe class UIHostingViewController : UIViewController
+    {
+		public UIHostingViewController (View view)
+			: this (view.GetSwiftHandle ())
+		{
+		}
+
+		public UIHostingViewController (SwiftHandle viewHandle)
+			: this (Init (viewHandle.Pointer, viewHandle.SwiftType.Metadata, viewHandle.SwiftType.GetProtocolConformance (SwiftUILib.ViewProtocol)))
+		{
+			// release extra ref added by Xamarin runtime
+			DangerousRelease ();
+		}
+
+		public UIHostingViewController (IntPtr handle)
+			: base (handle)
+		{
+		}
+
+		[DllImport (SwiftGlueLib.Path,
+			CallingConvention = CallingConvention.Cdecl,
+			EntryPoint = "swiftui_UIHostingController_rootView")]
+		static extern IntPtr Init (void* viewData, TypeMetadata* viewType, ProtocolWitnessTable* viewConformance);
+	}
+}
