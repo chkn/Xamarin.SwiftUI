3d942d0524b01c79816fe5317af306bd9a84ffb1
Alex Corrado <alexander.corrado@gmail.com>
2020-12-02 22:58:18 -0500

    Replace test projects with proper sample scaffolding
    The previous "sample" apps in this repo were more for testing
    the API bindings, and weren't good examples of how to use the framework.
    
    This starts the scaffolding for adding some real samples.
    
    Fixes #34

---
+++ samples/C#/Classic/iOS/AppDelegate.cs
@@ -0,0 +1,24 @@
+using Foundation;
+using UIKit;
+
+namespace CSharpSamples.Classic.iOS
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+
+		public override UIWindow Window {
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+
+			return true;
+		}
+	}
+}
---
+++ samples/C#/HelloView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SwiftUI;
+using static SwiftUI.Views;
+
+namespace CSharpSamples
+{
+	public partial record HelloView : View
+	{
+		readonly State<int> clicks = new (0);
+
+		public View Body =>
+			Button ($"Clicked {clicks.Value} times", () => clicks.Value++);
+	}
+}
