777171474912ce4fa02ec02496f90a13cefe86fb
Dominique Louis <savagesoftware@gmail.com>
2022-06-24 19:40:37 +0100

    Update Sample to reference ViewModifier.

--- samples/C#/HelloView.cs
+++ samples/C#/HelloView.cs
@@ -6,9 +6,31 @@ namespace CSharpSamples
 {
 	public partial record HelloView : View
 	{
-		readonly State<int> clicks = new (0);
+		readonly State<int> clickCounter = new (0);
 
-		public View Body =>
-			Button ($"Clicked {clicks.Value} times", () => clicks.Value++);
+		public record ButtonModifier : ViewModifier<ModifiedBackground<Content, Color>>
+		{
+			public override ModifiedBackground<Content, Color> Body (Content content)
+			{
+				return content.Background (Color.Red);
+			}
+		}
+
+		public ModifiedBackground<Button<Text>, ModifiedBackground<Text, Color>> Body {
+			get {
+				var button = new Button<Text> (
+					() => {
+						clickCounter.Value++;
+					}, new Text (string.Format (clickCounter.Value > 0 ? "Clicked {0} times" : "Never been clicked", clickCounter.Value))
+				);
+
+				// TODO button.Modifier (new ButtonModifier ());
+
+				var backgroundColour = clickCounter.Value > 0 ? clickCounter.Value % 2 == 0 ? Color.Red : Color.Blue : Color.Yellow;
+				var textColour = clickCounter.Value > 0 ? clickCounter.Value % 2 == 0 ? nameof (Color.Red) : nameof (Color.Blue) : nameof (Color.Yellow);
+
+				return button.Background (new Text (textColour).Background (backgroundColour));
+			}
+		}
 	}
 }
